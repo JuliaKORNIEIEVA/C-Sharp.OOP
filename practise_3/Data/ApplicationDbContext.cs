3a0ab4172a37231dcc97e97ba897c19c34c40258
JuliaKORNIEIEVA <kornieieva.juliia@gmail.com>
2024-12-16 19:41:53 +0200

    practice_4-5

---
+++ practise_3/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication1.Models.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+
+        }
+
+        public DbSet<Vehicle> Vehicle { get; set; }
+    }
+}
---
+++ practise_3/Models/Dto/VehicleDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models.Dto
+{
+    public class VehicleDTO
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+    }
+}
